d1ffefe48a419ae12f1ef031901b94b3f50dc31b
GGorAA <61655029+GGorAA@users.noreply.github.com>
2020-09-24 19:37:32 +0300

    Add settings, includes, and some variables

--- fishfeeder/fishfeeder.ino
+++ fishfeeder/fishfeeder.ino
@@ -1,7 +1,34 @@
-void setup() {
+// --------------------------Settings-------------------------- //
 
+#define SERVO_PIN 6               // Servo pin
+#define FEED_FREQUENCY 172800000  // Feed frequency
+#define FEED_AMOUNT 5             // Angle, to which servo will turn to open door, from which food will drop into aquarium
+#define LIGHT_STRIP_LED_COUNT 30  // Amount of LEDs on light strip
+
+// --------------------------Includes-------------------------- //
+
+#include "FastLED.h"      // FastLED library for light strip
+#include "ServoSmooth.h"  // Servo
+
+// --------------------------Main variables-------------------------- //
+
+/*
+ * Objects
+ */
+ServoSmooth mainFeederServo(180);
+CRGB lightStrip[];
+
+/*
+ * Variables
+ */
+
+unsigned long timePassedSinceLastFeed;  // Stores time in milliseconds since last feed
+
+// --------------------------Code-------------------------- //
+
+void setup() {
+    mainFeederServo.attach(SERVO_PIN, 0);  // Attach the servo and move it to starting angle
 }
 
 void loop() {
-    
 }
